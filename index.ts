3df432e48a17ee65839a0a58220bba55357b2acf
rodolphe37 <rodolphe.a@gmail.com>
2020-07-13 10:54:15 +0200

    Basic Deno Web Application

--- index.ts
+++ index.ts
@@ -3,8 +3,9 @@ import { Application } from "https://deno.land/x/abc@v1.0.0-rc10/mod.ts";
 const app = new Application();
 
 app
-  .get(":hello", (c) => {
-    return "Hello, Abc !";
+  .get("/hello", (c) => {
+    return "Hello World";
   })
 
+  .static("/", "./public")
   .start({ port: 8000 });
